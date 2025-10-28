dc2dc33d8866b899f4b53fbf8f562a6389d15c62
shanni <wushanni088@gmail.com>
2025-10-28 16:11:08 +0100

    sfdvsvsv

---
+++ README.md
@@ -0,0 +1,7 @@
+Scrivere un programma in linguaggio C che utilizzi una struttura per rappresentare un cocktail, con i seguenti attributi:
+Nome del cocktail (stringa), Prezzo (float) e Percentuale di alcol (short o float)
+
+Il programma deve:
+Creare una funzione stampa() che riceve un cocktail e ne visualizza le informazioni in formato leggibile.
+Creare almeno un cocktail e assegnare manualmente i valori ai campi.
+Stampare il cocktail usando la funzione stampa().
--- main.c
+++ main.c
@@ -2,17 +2,19 @@
 #include <stdlib.h>
 
 struct Cocktail {       //nel struct non ci sono metodi
-    char *name;
+    char *name;         //puntatore a una stringa
     float prezzo;
-    short alcol;
-};
+    short alcol;        //short -> intero corto
+} Cocktail;         //ripetuto alla fine vuol dire che non devi er forza scrivere struct Cocktail
 
-
+void stampa(struct Cocktail c) {
+    printf("Nome: %s \nPrezzo: %.2f euro \nAlcol: %d \n", c.name, c.prezzo, c.alcol);
+}
 
 int main(void) {
-    struct Cocktail mojito;
-    mojito.name = "Mojito";
-    mojito.prezzo = 3.50f;
+    struct Cocktail mojito;         //variabile mojito
+    mojito.name = "Mojito";         //assegno il nome
+    mojito.prezzo = 3.50f;          //prezzo in float
     mojito.alcol = 10;
 
     stampa(mojito);
